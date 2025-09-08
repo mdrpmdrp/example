08555488fe76e38c7e884768c054a6508e0d3cbc
mdrp <mdrpdeveloper@gamil.com>
2025-09-08 19:49:02 +0700

    Enhance attendance system UI and functionality
    - Updated page title and added favicon for better branding.
    - Improved card header styling with a gradient background.
    - Modified button styles to use gradients for a modern look.
    - Enhanced upload area with improved hover effects and functionality.
    - Added logo image to the card header for better visual identity.
    - Streamlined photo upload process and improved user feedback.
    - Refactored location handling to improve accuracy and user experience.
    - Integrated Telegram notification functionality for check-in/out events.
    - Cleaned up unused code and optimized existing functions for better performance.

--- workplace/jobs/timeattendent/code.gs.js
+++ workplace/jobs/timeattendent/code.gs.js
@@ -1,11 +1,19 @@
+const MAIN_FOLDER_ID = '1mjyrYs_9rd3llex15KAzWD_QcL7_xFzM';
 function doGet() {
     let html = HtmlService.createTemplateFromFile('index');
-    html.site_url = ScriptApp.getService().getUrl();
+    html.site_url = getScriptUrl();
     return html.evaluate()
         .setTitle('ระบบลงเวลาทำงาน')
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
         .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1');
+        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+        .setFaviconUrl('https://img2.pic.in.th/pic/sivathai-logo.th.png');
+}
+
+function getScriptUrl() {
+    let url = ScriptApp.getService().getUrl();
+    url = url.split('.com/').join('.com/a/*/');
+    return url;
 }
 
 function authenticateUser(empId = '11111', pin = '1234') {
@@ -64,3 +72,96 @@ function checkInUser(data) {
     sheet.appendRow(newRow);
     return JSON.stringify({ success: true, timestamp: timestamp });
 }
+
+function checkOutUser(data) {
+    let { empCode, photoFile, checkOutTime, location, currentUser, accuracy } = data
+    let ss = SpreadsheetApp.getActiveSpreadsheet();
+    let sheet = ss.getSheetByName('Punches');
+    let timestamp = new Date();
+    let uploadPhoto = uploadPhotoFile(photoFile, empCode, currentUser.name, currentUser.siteName, timestamp);
+    let newRow = [
+        timestamp,
+        empCode,
+        currentUser.name,
+        currentUser.team,
+        'OUT',
+        currentUser.siteId || '',
+        location.latitude || '',
+        location.longitude || '',
+        accuracy,
+        uploadPhoto ? 'https://lh3.googleusercontent.com/d/' + uploadPhoto.getId() : ''
+    ]
+    sheet.appendRow(newRow);
+    return JSON.stringify({ success: true, timestamp: timestamp, photoUrl: uploadPhoto ? 'https://lh3.googleusercontent.com/d/' + uploadPhoto.getId() : '' });
+}
+
+function uploadPhotoFile(photoFile, empCode, empName, siteName, timestamp) {
+    const getFolder = function (root, f_name) {
+        let folder = root.getFoldersByName(f_name);
+        if (!folder.hasNext()) {
+            folder = root.createFolder(f_name);
+        } else {
+            folder = folder.next();
+        }
+        return folder;
+    }
+    if (!photoFile) return null;
+    let folderId = MAIN_FOLDER_ID;
+    let mainFolder = DriveApp.getFolderById(folderId);
+    let siteFolder = getFolder(mainFolder, siteName);
+    timestamp = new Date(timestamp);
+    let dateText = Utilities.formatDate(timestamp, Session.getScriptTimeZone(), 'yyyy-MM-dd');
+    let datefolder = getFolder(siteFolder, dateText);
+    let f_name = photoFile.name
+    let f_type = photoFile.type
+    let f_dataurl = photoFile.dataUrl
+    let base64Data = f_dataurl.split(',')[1];
+    let blob = Utilities.newBlob(Utilities.base64Decode(base64Data), f_type, '[OUT]_' + empCode + '_' + empName + '_' + f_name);
+    let file = datefolder.createFile(blob);
+    return file
+}
+
+function sendTelegramNotification(data) {
+    const BOT_TOKEN = '7372234796:AAHP2Wxs3jAZggbEG4K7glvFBhojDq-MSck'; // Store securely
+    const CHAT_ID = '1354847893';
+
+    const locationInfo = data.location ?
+        // `📍 ตำแหน่ง: ${data.location.latitude.toFixed(6)}, ${data.location.longitude.toFixed(6)}` :
+        `📍 ตำแหน่ง: <a href="https://www.google.com/maps/search/?api=1&query=${data.location.latitude},${data.location.longitude}" target="_blank">ดูแผนที่</a>
+    (ความแม่นยำ: ${data.accuracy.toFixed(2)} เมตร)` :
+        '📍 ตำแหน่ง: ไม่สามารถระบุได้';
+
+    let message = `🔔 แจ้งเตือนลงเวลา <b>${data.action == "IN" ? "เข้า" : "ออก"}งาน</b>\n\n` +
+        `👤 พนักงาน: <b>${data.currentUser.name}</b>\n\n` +
+        `🆔 รหัส: <b>${data.currentUser.empCode}</b>\n\n` +
+        `🏢 ไซต์: <b>${data.currentUser.siteName}</b>\n\n` +
+        `⏰ เวลา: <b>${new Date().toLocaleString('th-TH', { timeZone: 'Asia/Bangkok' })}</b>\n\n` +
+        locationInfo;
+    let photoUrl = data.currentUser.photo || false;
+    if(photoUrl){
+        const response = UrlFetchApp.fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            payload: JSON.stringify({
+                chat_id: CHAT_ID,
+                photo: photoUrl,
+                caption: message,
+                parse_mode: 'HTML'
+            })
+        });
+        return JSON.stringify({ success: true, response: response.getContentText() });
+    }
+    else{
+        const response = UrlFetchApp.fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            payload: JSON.stringify({
+                chat_id: CHAT_ID,
+                text: message,
+                parse_mode: 'HTML'
+            })
+        });
+        return JSON.stringify({ success: true, response: response.getContentText() });
+    }
+}
+
